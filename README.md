0b60198aec0bbee6360919ee81a45044e00f9f18
tellerhq <devx@teller.io>
2024-05-17 05:59:38 +0000

    Release

--- README.md
+++
@@ -1,1 +0,0 @@
-# Teller Connect for iOS and macOS
---
+++ TellerKit.xcframework/ios-arm64/TellerKit.framework/Headers/TellerConnect.h
@@ -0,0 +1,18 @@
+//
+//  Teller.h
+//  Teller
+//
+//  Created by Stevie Graham on 16/07/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Teller.
+FOUNDATION_EXPORT double TellerVersionNumber;
+
+//! Project version string for Teller.
+FOUNDATION_EXPORT const unsigned char TellerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Teller/PublicHeader.h>
+
+
